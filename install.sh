d8fcd1e23b867f067c654e477e0eab88be02b902
Tilmann Meyer <allescrafterx@gmail.com>
2020-07-08 23:01:49 +0200

    fix: Install again

--- install.sh
+++ install.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 
 echo "---------- ATiltedTree's Dots ----------"
-echo "How do you want to install chezmoi?"
+echo "How do you want to install chezmoi?\n"
 
-select type in Arch Bin; do
-  case $type in
-  "Arch")
-    echo "---------- Installing chezmoi via Arch ----------"
-    sudo pacman -Sy chezmoi
-    echo "---------- Installing Dots ----------"
-    chezmoi init --apply https://github.com/ATiltedTree/dots.git
-    ;;
-  "Bin")
-    echo "---------- Installing chezmoi via Bin ----------"
-    curl -sfL https://git.io/chezmoi | sh
-    echo "---------- Installing Dots ----------"
-    ~/bin/chezmoi init --apply https://github.com/ATiltedTree/dots.git
-    ;;
-  *) echo "invalid option $REPLY" ;;
-  esac
-done
+echo "1) Arch Packages"
+echo "2) Binary download"
+printf "%s" "#?"
+
+read n
+case $n in
+1)
+  echo "---------- Installing chezmoi via Arch Packages ----------"
+  sudo pacman -Sy chezmoi
+  echo "---------- Installing Dots ----------"
+  chezmoi init --apply https://github.com/ATiltedTree/dots.git
+  ;;
+2)
+  echo "---------- Installing chezmoi via Binary download ----------"
+  curl -sfL https://git.io/chezmoi | sh
+  echo "---------- Installing Dots ----------"
+  ~/bin/chezmoi init --apply https://github.com/ATiltedTree/dots.git
+  ;;
+*) echo "invalid option" ;;
+esac
 
 echo "---------- Finished install ----------"
