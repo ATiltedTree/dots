afafa2c631c41a21b87a8ceccaf5694382b59502
Tilmann Meyer <allescrafterx@gmail.com>
2020-07-20 22:45:33 +0200

    docs(fish): Document everything

--- dot_config/fish/conf.d/00_xdg.fish
+++ dot_config/fish/conf.d/00_xdg.fish
@@ -10,16 +10,34 @@
 
 
 # Cache
+
+## CUDA
 set -Ux CUDA_CACHE_PATH $XDG_CACHE_HOME/cuda
 
 # Config
+
+## Docker
 set -Ux DOCKER_CONFIG $XDG_CONFIG_HOME/docker
+
+## GTK2
 set -Ux GTK2_RC_FILES $XDG_CONFIG_HOME/gtk-2.0/gtkrc
+
+## should be used anymore by KDE but it still does...
 set -Ux KDEHOME $XDG_CONFIG_HOME/kde
+
+## NPM
 set -Ux NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/npmrc
 
 # Data
+
+## Cargo
 set -Ux CARGO_HOME $XDG_DATA_HOME/cargo
+
+## GPG
 set -Ux GNUPGHOME $XDG_DATA_HOME/gnupg
+
+## Go
 set -Ux GOPATH $XDG_DATA_HOME/go
+
+## Wine
 set -Ux WINEPREFIX $XDG_DATA_HOME/wine/prefixes/default
