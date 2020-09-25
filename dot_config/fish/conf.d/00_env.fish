f09f3fc7615dbcc8060426bd2d6b98e60f73b0b2
Tilmann Meyer <allescrafterx@gmail.com>
2020-09-25 22:46:15 +0200

    chore(sway): No more gnome and move sway services into systemd

--- dot_config/fish/conf.d/00_env.fish
+++ dot_config/fish/conf.d/00_env.fish
@@ -9,6 +9,13 @@ set -gx EDITOR nvim
 # No lesshst
 set -gx LESSHISTFILE -
 
+# Setup sway
+set TTY1 (tty)
+if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
+  set -x XDG_SESSION_TYPE wayland
+  set -x XDG_SESSION_DESKTOP sway
+end
+
 # Wayland specific settings
 switch $XDG_SESSION_TYPE
   case wayland
@@ -16,8 +23,12 @@ switch $XDG_SESSION_TYPE
     set -gx SDL_VIDEODRIVER wayland
     set -gx QT_QPA_PLATFORMTHEME qt5ct
     set -gx QT_QPA_PLATFORM wayland
+    set -gx _JAVA_AWT_WM_NONREPARENTING 1
   case x11
 
   case tty
 
 end
+
+# Import into systemd
+systemctl --user import-environment SSH_AUTH_SOCK
