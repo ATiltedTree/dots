29258ed3cca9b4abb1067b2a1a1b41bd7c58f9f4
Tilmann Meyer <allescrafterx@gmail.com>
2021-04-05 01:46:20 +0200

    chore(fish): Cleaner PATH

--- dot_config/fish/conf.d/00_env.fish
+++ dot_config/fish/conf.d/00_env.fish
@@ -7,7 +7,9 @@ if test $XDG_SESSION_DESKTOP = "sway"
 end
 
 # PATH
-set -U fish_user_paths $HOME/.local/bin $CARGO_HOME/bin
+fish_add_path ~/.local/bin
+fish_add_path $CARGO_HOME/bin
+fish_add_path $FLUTTER_HOME/bin
 
 # Greeting
 set fish_greeting
